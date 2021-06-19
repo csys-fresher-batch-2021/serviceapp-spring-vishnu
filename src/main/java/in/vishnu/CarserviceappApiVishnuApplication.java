dbd4da0204cd55277f52f335baab9a4ee609efd5
Vishnu R <vishnu.ravi.543@gmail.com>
2021-06-19 11:01:16 +0530

    initial commit

---
+++ src/main/java/in/vishnu/CarserviceappApiVishnuApplication.java
@@ -0,0 +1,13 @@
+package in.vishnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarserviceappApiVishnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarserviceappApiVishnuApplication.class, args);
+	}
+
+}
